4e8e4118ec7fec2805f8a63e3571775506c77a3b
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-10 19:56:59 +0530

    changes

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+console.time("your code took");
+console.log("hello console");
+console.log(45 + 56);
+console.log(true);
+console.log([34, 2, 5, 8]);
+console.log({ abhishek: "this", marks: 34 });
+console.table({ abhishek: "this", marks: 34 });
+console.warn("this is warning");
+console.timeEnd("your code took");
+// console.assert(566 < 189, "Age > 189 is not possible");
+console.error("this is an error");
