09ecdd3ce9befd61a06b23206d23e2302bc15861
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-20 22:21:16 +0530

    changes

--- JS/tut11.js
+++ JS/tut11.js
@@ -1,3 +1,12 @@
 console.log("this is tutorial 11");
-let a = window;
+let a = window.document;
+// window.alert("hello harry");
+// a = prompt("this will destroy your computer");
+// a = confirm("are you sure you want to delete this page?");
+a = window.innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location.toString();
+a = window.history;
 console.log(a);
