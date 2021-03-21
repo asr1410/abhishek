456b05835150e02d3adfb355cf281bbba2caec93
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-03-21 22:31:43 +0530

    changes

--- JS/tut15.js
+++ JS/tut15.js
@@ -1,1 +1,6 @@
 console.log("this is tut 15");
+let cont = document.querySelector(".no");
+cont = document.querySelector(".container");
+console.log(cont.childNodes[1].nodeName);
+// console.log(cont.childNodes);
+// console.log(cont.children);
